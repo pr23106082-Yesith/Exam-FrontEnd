4a47c9cdba515d400ba3019600a9714f88676847
pr23106082-Yesith <hansana.etec1216@example.com>
2024-07-26 14:01:23 +0530

    AddCustomerDetails

--- src/app/pages/manage-customer/manage-customer.component.ts
+++ src/app/pages/manage-customer/manage-customer.component.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-manage-customer',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent ,HttpClientModule ,FormsModule, CommonModule],
   templateUrl: './manage-customer.component.html',
   styleUrl: './manage-customer.component.css'
 })
 export class ManageCustomerComponent {
 
+  public CustomerOb ={
+    name : "" ,
+    city :  "",
+    contact : ""
+    }
+
+  constructor(private http :HttpClient){}
+
+
+  AddCustomer(){
+    this.http.post("http://localhost:8080/customer/add-customer",this.CustomerOb).subscribe(
+      (data)=>{
+        console.log(data);
+        Swal.fire({
+          title: "Customer Added!",
+          text: "You clicked the button!",
+          icon: "success"
+        });
+      }
+    )
+  }
 }
